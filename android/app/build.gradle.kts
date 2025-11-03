4eaf14e5af987cf901aa48712526b48f0c3bbdf0
Marcelckp <81308023+Marcelckp@users.noreply.github.com>
2025-11-03 20:35:57 +0200

    chore: android runtime crash fixed

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -23,7 +23,7 @@ android {
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         
         // API base URL
-        buildConfigField("String", "API_BASE_URL", "\"http://10.0.2.2:3000/api\"")
+        buildConfigField("String", "API_BASE_URL", "\"http://10.0.2.2:3000/api/\"")
     }
 
     buildTypes {
